b58fb369319d37ef1a97679cb2d11648b6ba9769
Your Name <you@example.com>
2021-08-26 00:36:48 -0500

    create fronted

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Ciudades
+from .models import Ciudades, Distancias, Zonas, Tarifas
 # Create your views here.
 
 def home(request):
@@ -8,5 +8,84 @@ def home(request):
     return render(request, 'pages/home.html', context)
 
 def info(request):
+    ciudades = Ciudades.objects.all()
+    distancias = Distancias.objects.all()
     if request.method == 'POST':
-        pass
+        origenRe = request.POST['origen']
+        destinoRe = request.POST['destino']
+        alto = request.POST['alto']
+        ancho = request.POST['ancho']
+        largo = request.POST['largo']
+        peso = request.POST['peso']
+
+        origen = Ciudades.objects.get(nombre=origenRe)
+        destino = Ciudades.objects.get(nombre=destinoRe)
+
+        if origen and destino:
+            distancia = Distancias.objects.filter(
+                ciudad1=origen.id
+            ).filter(
+                ciudad2=destino.id
+            ).distinct()
+        
+            distancia = distancia[0].distancia
+
+        if distancia:
+            zona = Zonas.objects.filter(min__lte=distancia).filter(max__gte=distancia)
+            zona = zona[0].categoria
+        
+        pesovolumetrico = round((int(largo) * int(ancho) * int(alto)) / 5000)
+        pesoreal = round(int(peso))
+        
+        pesoAbsoluto = 0
+        if pesovolumetrico >= pesoreal:
+            pesoAbsoluto = pesovolumetrico
+        else:
+            pesoAbsoluto = pesoreal
+
+        tarifa1 = Tarifas.objects.filter(
+            proveedor=1
+        ).filter(
+            kg__gte=pesoAbsoluto
+        ).filter(
+            zona=zona
+        )
+
+        tarifa2 = Tarifas.objects.filter(
+            proveedor=2
+        ).filter(
+            kg__gte=pesoAbsoluto
+        ).filter(
+            zona=zona
+        )
+
+        tarifa3 = Tarifas.objects.filter(
+            proveedor=3
+        ).filter(
+            kg__gte=pesoAbsoluto
+        ).filter(
+            zona=zona
+        )
+
+        proveedor1 = tarifa1[0].precio
+        proveedor2 = tarifa2[0].precio
+        proveedor3 = tarifa3[0].precio
+                
+        context = {
+            'ciudades':ciudades,
+            'distancia': distancia,
+            'pesovolumetrico': pesovolumetrico,
+            'pesoreal': pesoreal,
+            'alto': alto,
+            'ancho': ancho,
+            'largo': largo, 
+            'zona': zona,
+            'origen': origenRe,
+            'destino': destinoRe,
+            'proveedor1': proveedor1,
+            'proveedor2': proveedor2,
+            'proveedor3': proveedor3,
+        }
+
+        return render(request, 'pages/home.html', context)
+
